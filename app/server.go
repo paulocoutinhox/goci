40311cab479c930ea0c1d7f4b99a3f335dd81d3a
Paulo Coutinho <paulo@prsolucoes.com>
2016-05-20 22:33:54 -0300

    better server error checking when app start

--- app/server.go
+++ app/server.go
@@ -106,6 +106,11 @@ func (This *WebServer) CreateStructure() {
 }
 
 func (This *WebServer) Start() {
+	err := This.Router.Run(This.Host)
+
+	if err != nil {
+		log.Fatalf("Server not started: %v", err)
+	}
+
 	log.Printf("Server started on %v : OK", This.Host)
-	This.Router.Run(This.Host)
 }
