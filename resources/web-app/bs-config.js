675d3406d70f01b0536c30574b7a8e762fc759e5
Paulo Coutinho <paulo@prsolucoes.com>
2016-08-11 02:37:48 -0300

    support for angular2

--- resources/web-app/bs-config.js
+++ resources/web-app/bs-config.js
@@ -2,7 +2,7 @@ var proxy = require('http-proxy-middleware');
 
 var apiProxy = proxy('/api', {
 	target: 'http://localhost:8080',
-	changeOrigin: false
+	changeOrigin: true
 });
 
 module.exports = {
